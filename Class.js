a3852f9bb07ccb094b546235024763e45f50850e
Ragukumaran Piratheep <rpiratheep2002@gmail.com>
2023-10-31 15:47:57 +0530

    added

---
+++ Class.js
@@ -0,0 +1,25 @@
+// create a class name= person
+class Person {
+
+    constructor( ID,name, age) {
+        this.ID = ID;
+        this.Name = name;
+        this.Age = age;
+        }
+        // write a function to call object
+        
+    }
+            //create an array of objects
+            let people = [new Person("1", "John", 25), new Person ("2","Jane",30)];
+
+
+
+    
+        
+
+
+
+
+
+
+
